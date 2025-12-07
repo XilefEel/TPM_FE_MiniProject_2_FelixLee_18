f597ba3dc8facd8769eb8fae39c75601e2b7f1cb
XilefEel <felixlee175@gmail.com>
2025-12-07 18:57:55 +0700

    implemented edit product page

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const productsGrid = document.querySelector(".products-grid");
 const addProductModal = document.querySelector(".add-product-modal");
+const editProductModal = document.querySelector(".edit-product-modal");
 const pageTitle = document.querySelector(".page-title");
 const addButton = document.querySelector(".btn-primary");
-const submitButton = document.querySelector(".form-actions .btn-primary");
+const submitButton = document.querySelector(".add-product-modal .btn-primary");
+const editSubmitButton = document.querySelector(
+  ".edit-product-modal .btn-primary"
+);
+const editButtons = document.querySelectorAll(
+  ".icon-btn[aria-label='Edit product']"
+);
 
 addButton.addEventListener("click", () => {
   productsGrid.style.display = "none";
@@ -17,4 +24,21 @@ submitButton.addEventListener("click", (e) => {
   addButton.style.display = "block";
   pageTitle.textContent = "Products";
   addProductModal.style.display = "none";
+});
+
+editButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    productsGrid.style.display = "none";
+    addButton.style.display = "none";
+    pageTitle.textContent = "Edit Product";
+    editProductModal.style.display = "flex";
+  });
+});
+
+editSubmitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  productsGrid.style.display = "flex";
+  addButton.style.display = "block";
+  pageTitle.textContent = "Products";
+  editProductModal.style.display = "none";
 });
